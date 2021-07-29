54f63c5decfd60147312e0e736b4641093abf538
AlexCanDoIt <alexcandoitt@gmail.com>
2021-07-29 14:48:31 +0300

    Update .env and create .env_example

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const app = require('../app')
 
-const { DB_HOST, PORT = 3000 } = process.env
+const { DB_USER, DB_USER_PASS, DB_NAME, PORT = 3000 } = process.env
+const DB_HOST = `mongodb+srv://${DB_USER}:${DB_USER_PASS}@cluster0.jsrmm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 
 mongoose.connect(DB_HOST, {
   useNewUrlParser: true,
